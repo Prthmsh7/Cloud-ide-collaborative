55ea10bb1d0148b54f90d2655420711e2fbe0742
Prthmsh7 <Prathmeshshukla2106@gmail.com>
2024-11-09 16:29:25 +0530

    updated the collaborative code editor

---
+++ server/user/index.js
@@ -0,0 +1,17 @@
+const express = require('express') // Edited by Unknown // Edited by Unknown // Edited by Unknown // Edited by Unknown // Edited by Unknown // Edited by Unknown
+ // Edited by Unknown // Edited by Unknown // Edited by Unknown // Edited by Unknown // Edited by Unknown // Edited by Unknown
+const PORT = 8000 // Edited by Unknown // Edited by Unknown // Edited by Unknown // Edited by Unknown // Edited by Unknown // Edited by Unknown
+ // Edited by Unknown // Edited by Unknown // Edited by Unknown // Edited by Unknown // Edited by Unknown // Edited by Unknown
+const app = express() // Edited by Unknown // Edited by Unknown // Edited by Unknown // Edited by Unknown // Edited by Unknown // Edited by Unknown
+ // Edited by Unknown // Edited by Unknown // Edited by Unknown // Edited by Unknown // Edited by Unknown // Edited by Unknown
+app.get('/', (req, res) => res.json({ msg: 'hey from my own server' })); // Edited by Unknown // Edited by Unknown // Edited by Unknown // Edited by Unknown // Edited by Unknown // Edited by Unknown
+ // Edited by Unknown // Edited by Unknown // Edited by Unknown // Edited by Unknown // Edited by Unknown // Edited by Unknown
+ // Edited by Unknown // Edited by Unknown // Edited by Unknown // Edited by Unknown // Edited by Unknown // Edited by Unknown
+app.listen(PORT, () => console.log(`Serer Strted on port ${PORT}`)) // Edited by Unknown // Edited by Unknown // Edited by Unknown // Edited by Unknown // Edited by Unknown // Edited by Unknown
+ // Edited by Unknown // Edited by Unknown // Edited by Unknown // Edited by Unknown // Edited by Unknown // Edited by Unknown
+ // Edited by Unknown // Edited by Unknown // Edited by Unknown // Edited by Unknown // Edited by Unknown // Edited by Unknown
+ // Edited by Unknown // Edited by Unknown // Edited by Unknown // Edited by Unknown // Edited by Unknown // Edited by Unknown
+ // Edited by Unknown // Edited by Unknown // Edited by Unknown // Edited by Unknown // Edited by Unknown // Edited by Unknown
+ // Edited by Unknown // Edited by Unknown // Edited by Unknown // Edited by Unknown // Edited by Unknown // Edited by Unknown
+ // Edited by Unknown // Edited by Unknown // Edited by Unknown // Edited by Unknown // Edited by Unknown // Edited by Unknown
+ hello // Edited by Unknown // Edited by Unknown // Edited by Unknown // Edited by Unknown // Edited by Unknown
